8b3d8d0383ae2422c4df52d8bc992dac53629a7e
ram2005024 <sharmashekhar20050@gmail.com>
2026-08-10 00:18:00 +0545

    backend root structure created

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from app.apis.base_router import include_base_router
 from app.exceptions.exception_handler import exception_handler_caller
 from app.middlewares.logger_middleware import LoggingMiddleware
 
-app = FastAPI(title="sahayog")git add .
+app = FastAPI(title="sahayog")
 
 include_base_router(app)
 exception_handler_caller(app)
